b3f63c05b28c10114c6095c1d30bdb2e937c36fa
Carl Dawson <carl@invisible-gorilla.com>
2022-02-06 22:54:51 -0500

    Added login/logout to users service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { users } from '../data/users';
+
 @Injectable({
   providedIn: 'root'
 })
-export class UserService { }
+export class UserService {
+  login(): void {
+
+  }
+
+  logout(): void {
+
+  }
+}
